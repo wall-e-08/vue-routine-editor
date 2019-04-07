89ded62ca109ff972bd5586fafc33444aec8bc0d
Debashis Roy Bhowmik <debashis.buet08@gmail.com>
2019-04-08 02:24:39 +0600

    init

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <!--<img src="./assets/logo.png">-->
+    <RoutineBoard/>
+  </div>
+</template>
+
+<script>
+import RoutineBoard from './components/RoutineBoard'
+
+export default {
+    name: 'App',
+    components: {
+        RoutineBoard
+    }
+}
+</script>
+
+<style>
+#app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+}
+</style>
---
+++ src/components/RoutineBoard.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>{{ txt }}</div>
+</template>
+
+<script>
+    export default {
+        name: "RoutineBoard",
+        data: function() {
+            return {
+              txt: "test",
+            }
+      }
+  }
+</script>
+
+<style scoped>
+
+</style>
